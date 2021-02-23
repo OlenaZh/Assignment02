4bf5a8e95d0cb8383828a7bb8207b7772e927d56
OlenaZh <alyona.mesh@gmail.com>
2021-02-23 14:49:21 -0800

    translate Code Changed

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,4 +1,13 @@
 var translateCode = window.prompt ("Please enter a language code");
 if (translateCode = es) {
-    document.write("Hello World translated in Esperando is: Saluton mondo");
+    window.document.write("Hello World translated in Esperando is: Saluton mondo");
+} else if (translateCode = de) {
+    window.document.write("Hello World translated in German is: Hallo Welt");
+} else if (translateCode = fr) {
+    window.document.write("Hello World translated in Franch is: Bonjour le monde");
+} else if (translateCode = en) {
+    window.document.write("Hello World translated in English is: Hello World");
 }
+
+
+
