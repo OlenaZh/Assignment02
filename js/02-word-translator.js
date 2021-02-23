747c434f71813071bba548aa1d08681573a7a95e
OlenaZh <alyona.mesh@gmail.com>
2021-02-23 14:56:01 -0800

    eslint added

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,3 +1,4 @@
+/*eslint-env browser*/
 var translateCode = window.prompt ("Please enter a language code");
 if (translateCode = es) {
     window.document.write("Hello World translated in Esperando is: Saluton mondo");
