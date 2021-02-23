bcb74b69953a1896510ee1af2b447caa535fe7c8
OlenaZh <alyona.mesh@gmail.com>
2021-02-23 15:07:48 -0800

    translate changes

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -2,7 +2,7 @@
 var translateCode = window.prompt ("Please enter a language code");
 if (translateCode = 'es') {
     window.document.write("Hello World translated in Esperando is: Saluton mondo");
-} else if (translateCode = 'de') {
+}  else if (translateCode = 'de') {
     window.document.write("Hello World translated in German is: Hallo Welt");
 } else if (translateCode = fr) {
     window.document.write("Hello World translated in Franch is: Bonjour le monde");
