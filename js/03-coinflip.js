b61b9b27ae630adf439c8c196cd7a47c04e18988
OlenaZh <alyona.mesh@gmail.com>
2021-02-23 16:17:01 -0800

    coinFlip Changed

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -2,7 +2,7 @@
 var choice = prompt ("Heads or Tails");
 var coinFlip = Math.round(Math.random()) +1;
 var flipResult;
-if (coinFlip = 1) {
+if (coinFlip < 10) {
     var flipResult = "heads" ;
 } else {
     var flipResult = "tails";
