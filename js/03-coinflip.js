4f744b3f1cbb25b0e338a757536ba2acdec5e722
OlenaZh <alyona.mesh@gmail.com>
2021-02-23 16:12:40 -0800

    coinFlio changes

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,22 +1,22 @@
 /*eslint-env browser*/
-var coinFlip = Math.round(Math.random());
-var choice = window.prompt ("Heads or Tails");
+var choice = prompt ("Heads or Tails");
+var coinFlip = Math.round(Math.random()) +1;
 var flipResult;
-if (coinFlip < 10) {
+if (coinFlip = 1) {
     var flipResult = "heads" ;
 } else {
     var flipResult = "tails";
 } if (flipResult == choice) {
     if (flipResult == "heads") {
-        window.alert("The flip was heads and you chose heads...you win!");
+        alert("The flip was heads and you chose heads...you win!");
     } else {
-        window.alert("The flip was tails and you chose tails...you win!"); 
+        alert("The flip was tails and you chose tails...you win!"); 
     }
 } else {
     if (flipResult == "heads") {
-        window.alert("The flip was heads and you chose tails...you lose!"); 
+        alert("The flip was heads and you chose tails...you lose!"); 
     } else {
-        window.alert("The flip was tails and you chose heads...you lose!"); 
+        alert("The flip was tails and you chose heads...you lose!"); 
     }
 
 }
