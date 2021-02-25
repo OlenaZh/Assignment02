c300af03a59903380dd7877db80de9f5e591a845
OlenaZh <alyona.mesh@gmail.com>
2021-02-24 20:58:45 -0800

    translation correct

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,15 +1,15 @@
 /*eslint-env browser*/
 var translateCode = window.prompt ("Please enter a language code");
 if (translateCode = 'es') {
-    window.document.write("Hello World translated in Esperando is: Saluton mondo");
+    document.write ("Hello World translated in Esperando is: Saluton mondo");
 }  else if (translateCode = 'de') {
-    window.document.write("Hello World translated in German is: Hallo Welt");
-} else if (translateCode = fr) {
-    window.document.write("Hello World translated in Franch is: Bonjour le monde");
+  document.write ("Hello World translated in German is: Hallo Welt");
+} else if (translateCode = 'fr') {
+   document.write ("Hello World translated in Franch is: Bonjour le monde");
 } else if (translateCode = 'en') {
-    window.document.write("Hello World translated in English is: Hello World");
+   document.write ("Hello World translated in English is: Hello World");
 } else  {
-    window.document.write("Hello World translated in English is: Hello World");
+   document.write ("Hello World translated in English is: Hello World");
 }
 
 
