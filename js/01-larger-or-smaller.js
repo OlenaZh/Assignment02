5c52f0e86f8c9ae9ee2f4ec5297f24104cc89497
OlenaZh <alyona.mesh@gmail.com>
2021-02-24 20:36:38 -0800

    test done

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -8,4 +8,4 @@ if (parseInt(integer1, 10)  > parseInt(integer2, 10)) {
     document.write("The larger of "+ integer1+" and "+ integer2+ " is "+ integer2+ ".");
 } else {
     document.write("The values "+ integer1+ " and "+integer2+ " are equal.");
-}
+} 
