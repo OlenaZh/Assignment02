0ba4a2c0377345fb12f63abdb38bc108f59353cf
OlenaZh <alyona.mesh@gmail.com>
2021-02-24 18:26:08 -0800

    yes no added

--- js/05-hitchhikers-guide.js
+++ js/05-hitchhikers-guide.js
@@ -1,1 +1,8 @@
 /*eslint-env browser*/
+
+var play = window.prompt ("Are you ready to play?");
+if (play == "yes") {
+    alert ("You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.");
+} else {
+    document.write ("Thank you. Play again later")
+};
